e9d7897dd740bb0b4d95599d217c4bea1a021649
Jeffrey Washburn <jeffrey.s.washburn@outlook.com>
2022-05-20 13:21:51 -0700

    update docs

--- Set-EulaAccepted.ps1
+++ Set-EulaAccepted.ps1
@@ -1,3 +1,8 @@
+<#
+In order to run a Minecraft server, the EULA must be accepted - this script will do that
+This script is run as part of container build and does not need to be run manually.
+#>
+
 [CmdletBinding()]
 param(
     [Parameter(Mandatory = $true)]
