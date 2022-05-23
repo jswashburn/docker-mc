7c7e62193e0defa17e29068476502a09be4a1299
Jeffrey Washburn <jeffrey.s.washburn@outlook.com>
2022-05-23 14:25:54 -0700

    fix cleanup script

--- tools/remove-resource-groups.ps1
+++ tools/remove-resource-groups.ps1
@@ -1,1 +1,7 @@
+Get-AzADUser -ErrorVariable notSignedIn -ErrorAction SilentlyContinue
+
+if ($notSignedIn) {
+    Connect-AzAccount -UseDeviceAuthentication
+}
+
 Get-AzResourceGroup -Tag @{"docker-mc"=""} |  Remove-AzResourceGroup -Force -AsJob
